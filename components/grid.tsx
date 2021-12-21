e11c964c424d060a14c8412c5778b4d274cb6ce6
Aidan Timson <contact@timmo.xyz>
2021-12-21 11:21:44 +0000

    Cleanup

--- components/grid.tsx
+++ components/grid.tsx
@@ -5,33 +5,18 @@ import Moment from "react-moment";
 function GridComponent(): ReactElement {
   return (
     <>
-      <Grid
-        container
-        sx={{
-          height: "50%",
-          textAlign: "center",
-        }}>
+      <Grid container sx={{ height: "50%", textAlign: "center" }}>
         <Grid item xs={12}>
           <Typography component="span" variant="h2">
             <Moment format="HH:mm:ss" interval={500} />
           </Typography>
         </Grid>
       </Grid>
-      <Grid
-        container
-        alignItems="flex-end"
-        sx={{
-          height: "50%",
-        }}>
+      <Grid container alignItems="flex-end" sx={{ height: "50%" }}>
         <Grid item xs={6}>
           <Typography component="span" variant="h2"></Typography>
         </Grid>
-        <Grid
-          item
-          xs={6}
-          sx={{
-            textAlign: "right",
-          }}>
+        <Grid item xs={6} sx={{ textAlign: "right" }}>
           <Typography component="span" variant="h2"></Typography>
         </Grid>
       </Grid>

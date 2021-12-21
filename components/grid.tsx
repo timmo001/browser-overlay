4e60fb2b3c43ff5fffb9cbd33d3b445658a03e10
Aidan Timson <contact@timmo.xyz>
2021-12-21 11:27:30 +0000

    6 corners

--- components/grid.tsx
+++ components/grid.tsx
@@ -5,18 +5,27 @@ import Moment from "react-moment";
 function GridComponent(): ReactElement {
   return (
     <>
-      <Grid container sx={{ height: "50%", textAlign: "center" }}>
-        <Grid item xs={12}>
+      <Grid container sx={{ height: "50%" }}>
+        <Grid item xs={4}>
+          <Typography component="span" variant="h2"></Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ textAlign: "center" }}>
           <Typography component="span" variant="h2">
             <Moment format="HH:mm:ss" interval={500} />
           </Typography>
         </Grid>
-      </Grid>
-      <Grid container alignItems="flex-end" sx={{ height: "50%" }}>
-        <Grid item xs={6}>
+        <Grid item xs={4} sx={{ textAlign: "right" }}>
           <Typography component="span" variant="h2"></Typography>
         </Grid>
-        <Grid item xs={6} sx={{ textAlign: "right" }}>
+      </Grid>
+      <Grid container alignItems="flex-end" sx={{ height: "50%" }}>
+        <Grid item xs={4}>
+          <Typography component="span" variant="h2"></Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ textAlign: "center" }}>
+          <Typography component="span" variant="h2"></Typography>
+        </Grid>
+        <Grid item xs={4} sx={{ textAlign: "right" }}>
           <Typography component="span" variant="h2"></Typography>
         </Grid>
       </Grid>
